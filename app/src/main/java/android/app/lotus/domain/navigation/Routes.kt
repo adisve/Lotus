13db4b4eb4573c704c9a61ff95e14f2ab9aeadf7
adisve <adis.veletanlic@gmail.com>
2023-10-20 10:14:56 +0200

    Cleanup routes file

--- app/src/main/java/android/app/lotus/domain/navigation/Routes.kt
+++ app/src/main/java/android/app/lotus/domain/navigation/Routes.kt
@@ -2,13 +2,11 @@ package android.app.lotus.domain.navigation
 
 object Routes {
     const val home = "home"
-    const val profile = "profile"
     const val auth = "auth"
-    const val login = "login"
     const val stats = "stats"
     const val articles = "articles"
     const val articleDetail = "articleDetail/{articleId}"
     const val videos = "videos"
     const val videoDetail = "videoDetail/{videoId}"
-    const val evaluation = "managerEvaluation"
+    const val evaluation = "evaluation"
 }

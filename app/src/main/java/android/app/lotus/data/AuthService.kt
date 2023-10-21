14c6e964ca2f6902453aa3155669a592e5c57582
adisve <adis.veletanlic@gmail.com>
2023-10-21 11:22:50 +0200

    Add PreferenceService

--- app/src/main/java/android/app/lotus/data/AuthService.kt
+++ app/src/main/java/android/app/lotus/data/AuthService.kt
@@ -4,7 +4,9 @@ import android.app.lotus.app
 import io.realm.kotlin.mongodb.Credentials
 import io.realm.kotlin.mongodb.User
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class AuthService @Inject constructor() {
     suspend fun createAccount(email: String, password: String): Result<Unit> {
         return try {

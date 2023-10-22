fb8d15af17fda0cad249ea594fa0104535742a47
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-10-22 13:02:04 +0200

    Made the data load into articleVM

--- app/src/main/java/android/app/lotus/data/DataService.kt
+++ app/src/main/java/android/app/lotus/data/DataService.kt
@@ -2,20 +2,18 @@ package android.app.lotus.data
 
 import android.app.lotus.app
 import android.app.lotus.domain.models.article
+import android.util.Log
 import io.realm.kotlin.Realm
 import io.realm.kotlin.ext.query
+import io.realm.kotlin.internal.platform.runBlocking
 import io.realm.kotlin.mongodb.User
 import io.realm.kotlin.mongodb.exceptions.SyncException
-import io.realm.kotlin.mongodb.subscriptions
 import io.realm.kotlin.mongodb.sync.SyncConfiguration
 import io.realm.kotlin.mongodb.sync.SyncSession
 import io.realm.kotlin.mongodb.syncSession
+import io.realm.kotlin.notifications.ResultsChange
 import io.realm.kotlin.query.Sort
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -24,32 +22,30 @@ class DataService @Inject constructor(
     onSyncError: (session: SyncSession, error: SyncException) -> Unit
 ) : SyncRepository {
 
-    private val realm: Realm
-    private val config: SyncConfiguration
+    private lateinit var realm: Realm
+    private lateinit var config: SyncConfiguration
     private val currentUser: User
         get() = app.currentUser!!
 
     init {
-        config = SyncConfiguration.Builder(currentUser, setOf(article::class))
-            .errorHandler { session: SyncSession, error: SyncException ->
-                onSyncError.invoke(session, error)
-            }
-            .waitForInitialRemoteData()
-            .build()
-
-        realm = Realm.open(config)
-
-        // Mutable states must be updated on the UI thread
-        CoroutineScope(Dispatchers.Main).launch {
-            realm.subscriptions.waitForSynchronization()
+        runBlocking {
+            config = SyncConfiguration.Builder(currentUser, setOf(article::class))
+                .initialSubscriptions { realm ->
+                    add(
+                        realm.query<article>(),
+                        "article"
+                    )
+                }
+                .build()
+            realm = Realm.open(config)
+            Log.v("REALM", "Successfully opened realm: ${realm.configuration.name}")
         }
     }
 
-    override fun getArticleList(): Flow<List<article>> {
+    override fun getArticleList(): Flow<ResultsChange<article>> {
         return realm.query<article>()
             .sort(Pair("_id", Sort.ASCENDING))
             .asFlow()
-            .map { it.list }
     }
 
 
--- app/src/main/java/android/app/lotus/observables/ArticleViewModel.kt
+++ app/src/main/java/android/app/lotus/observables/ArticleViewModel.kt
@@ -1,17 +1,65 @@
 package android.app.lotus.observables
 
 import android.app.lotus.data.DataService
+import android.app.lotus.domain.models.article
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.realm.kotlin.notifications.InitialResults
+import io.realm.kotlin.notifications.ResultsChange
+import io.realm.kotlin.notifications.UpdatedResults
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ArticleViewModel @Inject constructor(
     private val dataService: DataService
 ) : ViewModel() {
+    val articleListState: SnapshotStateList<article> = mutableStateListOf()
 
     init {
-//        var t = dataService.getArticleList()
-//        Log.i("AuthViewModel", t.toString())
+        viewModelScope.launch {
+            dataService.getArticleList()
+                .collect { event: ResultsChange<article> ->
+                    when (event) {
+                        is InitialResults -> {
+                            articleListState.clear()
+                            articleListState.addAll(event.list)
+                            Log.i(
+                                "ArticleViewModel",
+                                "InitialResults: ${articleListState.toList().first().content}"
+                            )
+                        }
+
+                        is UpdatedResults -> {
+                            if (event.deletions.isNotEmpty() && articleListState.isNotEmpty()) {
+                                event.deletions.reversed().forEach {
+                                    articleListState.removeAt(it)
+                                }
+                            }
+                            if (event.insertions.isNotEmpty()) {
+                                event.insertions.forEach {
+                                    articleListState.add(it, event.list[it])
+                                }
+                            }
+                            if (event.changes.isNotEmpty()) {
+                                event.changes.forEach {
+                                    articleListState.removeAt(it)
+                                    articleListState.add(it, event.list[it])
+                                }
+                            }
+                            Log.i(
+                                "ArticleViewModel",
+                                "InitialResults: ${articleListState.toList()}"
+                            )
+                        }
+
+                        else -> Unit // No-op
+                    }
+                }
+        }
     }
 }

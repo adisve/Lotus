5e26ef0b8377ef84564895cec39ebc7d8eb4dec4
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-10-25 22:16:18 +0200

    Connect to backend

--- app/src/main/java/android/app/lotus/observables/StatisticsViewModel.kt
+++ app/src/main/java/android/app/lotus/observables/StatisticsViewModel.kt
@@ -1,9 +1,114 @@
 package android.app.lotus.observables
 
+import android.app.lotus.app
+import android.app.lotus.data.services.DataService
+import android.app.lotus.domain.models.realm.user
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.realm.kotlin.mongodb.ext.customDataAsBsonDocument
+import io.realm.kotlin.notifications.InitialResults
+import io.realm.kotlin.notifications.ResultsChange
+import io.realm.kotlin.notifications.UpdatedResults
+import kotlinx.coroutines.launch
+import org.mongodb.kbson.BsonString
 import javax.inject.Inject
+
+
+enum class StatisticsStatus {
+    Loading,
+    Empty,
+    Populated
+}
 
 @HiltViewModel
 class StatisticsViewModel @Inject constructor(
-) : ViewModel()
+    private val dataService: DataService
+) : ViewModel() {
+    private val _userList = mutableListOf<user>()
+    val userList: List<user> get() = _userList
+
+    private val _status = MutableLiveData<StatisticsStatus>(StatisticsStatus.Loading)
+    val status: LiveData<StatisticsStatus> get() = _status
+
+    private val user = app.currentUser!!
+    private val bsonData = user.customDataAsBsonDocument()!!
+
+    init {
+        observeCompanyUsersList()
+    }
+
+    private fun observeCompanyUsersList() {
+        viewModelScope.launch {
+            dataService.getCompanyUsers(
+                (bsonData.get("company") as BsonString).value
+            )
+                .collect { event: ResultsChange<user> ->
+                    handleCompanyUsersListEvent(event)
+                }
+        }
+    }
+
+    private fun handleCompanyUsersListEvent(event: ResultsChange<user>) {
+        Log.i("ArticleViewModel", "Event: $event")
+        when (event) {
+            is InitialResults -> {
+                updateInitialState(event.list)
+                _status.value =
+                    if (event.list.isEmpty()) StatisticsStatus.Loading else StatisticsStatus.Populated
+            }
+
+            is UpdatedResults -> {
+                updateChangedState(event)
+                _status.value =
+                    if (_userList.isEmpty()) StatisticsStatus.Empty else StatisticsStatus.Populated
+            }
+
+            else -> {
+                _status.value = StatisticsStatus.Loading
+            }
+        }
+    }
+
+
+    private fun updateInitialState(list: List<user>) {
+        _userList.clear()
+        _userList.addAll(list)
+        Log.i("ArticleViewModel", "InitialResults: ${_userList.toList()}")
+    }
+
+    private fun updateChangedState(event: UpdatedResults<user>) {
+        applyDeletions(event.deletions)
+        applyInsertions(event.insertions, event.list)
+        applyChanges(event.changes, event.list)
+
+        Log.i("ArticleViewModel", "UpdatedResult: ${_userList.toList()}")
+    }
+
+    private fun applyDeletions(deletions: IntArray) {
+        if (deletions.isNotEmpty() && _userList.isNotEmpty()) {
+            deletions.reversed().forEach { index ->
+                _userList.removeAt(index)
+            }
+        }
+    }
+
+    private fun applyInsertions(insertions: IntArray, list: List<user>) {
+        if (insertions.isNotEmpty()) {
+            insertions.forEach { index ->
+                _userList.add(index, list[index])
+            }
+        }
+    }
+
+    private fun applyChanges(changes: IntArray, list: List<user>) {
+        if (changes.isNotEmpty()) {
+            changes.forEach { index ->
+                _userList[index] = list[index]
+            }
+        }
+    }
+}
--- app/src/main/java/android/app/lotus/view/statistics/Statistics.kt
+++ app/src/main/java/android/app/lotus/view/statistics/Statistics.kt
@@ -1,5 +1,8 @@
 package android.app.lotus.view.statistics
 
+import android.app.lotus.observables.StatisticsStatus
+import android.app.lotus.observables.StatisticsViewModel
+import android.app.lotus.view.general.DotsPulsing
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,46 +11,67 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
 import com.patrykandpatrick.vico.compose.chart.Chart
 import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.compose.m3.style.m3ChartStyle
+import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
 import com.patrykandpatrick.vico.core.entry.entryModelOf
 
 @Composable
 fun Statistics(navController: NavHostController) {
+    val statisticsViewModel: StatisticsViewModel = hiltViewModel()
+    val status = statisticsViewModel.status.observeAsState(initial = StatisticsStatus.Loading)
     val chartEntryModel = entryModelOf(4f, 12f, 8f, 16f)
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    when (status.value) {
+        StatisticsStatus.Loading -> {
+            DotsPulsing()
+        }
 
-        Column(
-            horizontalAlignment = Alignment.Start,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 75.dp)
-                .padding(horizontal = 25.dp)
-        ) {
-            Text(
-                modifier = Modifier.padding(vertical = 25.dp),
-                style = MaterialTheme.typography.bodyMedium,
-                lineHeight = 30.sp,
-                text = "Company members that finished evaluation process:"
-            )
-            Chart(
-                chart = lineChart(),
-                model = chartEntryModel,
-                startAxis = rememberStartAxis(),
-                bottomAxis = rememberBottomAxis(),
-            )
+        StatisticsStatus.Empty -> {
+            DotsPulsing()
+        }
+
+        StatisticsStatus.Populated -> {
+            // Todo: use actual data...
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Column(
+                    horizontalAlignment = Alignment.Start,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 75.dp)
+                        .padding(horizontal = 25.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.padding(vertical = 25.dp),
+                        style = MaterialTheme.typography.bodyMedium,
+                        lineHeight = 30.sp,
+                        text = "Company members that finished evaluation process:"
+                    )
+                    ProvideChartStyle(chartStyle = m3ChartStyle()) {
+                        Chart(
+                            chart = lineChart(),
+                            model = chartEntryModel,
+                            startAxis = rememberStartAxis(),
+                            bottomAxis = rememberBottomAxis(),
+                        )
+                    }
+                }
+            }
         }
     }
 }
